e428aa3376b79b22b7a2f424ae3a82c3ca49c5b7
bgomiss <ditty-inning-0v@icloud.com>
2024-05-19 16:32:29 +0300

    Initial Commit

---
+++ OpenAIDemo/OpenAIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAIDemoApp.swift
+//  OpenAIDemo
+//
+//  Created by aycan duskun on 19.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct OpenAIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
